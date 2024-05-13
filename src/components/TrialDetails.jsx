e7f4f00097c5c4e89ee6a7bc896926a531d53832
Arash <arash8877@gmail.com>
2024-05-13 22:45:03 +0200

    finished styling of TrialDetails

--- src/components/TrialDetails.jsx
+++ src/components/TrialDetails.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect } from "react";
+import styles from "./TrialDetails.module.css";
 import { TrialContext } from "../contexts/TrialContext";
 import { useParams } from "react-router-dom";
 
@@ -12,7 +13,7 @@ const TrialDetails = () => {
     conditionsModule,
     statusModule,
     eligibilityModule,
-    contactsLocationsModule
+    contactsLocationsModule,
   } = protocolSection || {};
   const { briefTitle } = identificationModule || {};
   const { detailedDescription } = descriptionModule || {};
@@ -28,22 +29,42 @@ const TrialDetails = () => {
   }, [nctId, getTrial]);
 
   return (
-    <section>
-      <div>
-        <h2>{briefTitle}</h2>
-        <p>{detailedDescription}</p>
-        {keywords &&
-          keywords.map((condition, index) => {
-            return <p key={index}>{condition}</p>;
-          })}
+    <section className={styles.container}>
+      <div className={styles.header}>
+        <div className={styles.logoWrapper}>
+          <img
+            className={styles.logo}
+            src="https://images.unsplash.com/photo-1516876437184-593fda40c7ce?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="logo"
+          />
+        </div>
+        <div className={styles.userName}>user.name</div>
       </div>
-      <div>
-        <ul>
-          <li>ID: {nctId}</li>
-          <li>Date: {statusVerifiedDate}</li>
-          <li>Eligibility: {sex}</li>
-          <li>Location: {city},{state}</li>
-        </ul>
+      <div className={styles.main}>
+        <div className={styles.leftBar}>
+          <h4>{briefTitle}</h4>
+          <p>{detailedDescription}</p>
+          <div className={styles.tagContainer}>
+            {keywords &&
+              keywords.map((condition, index) => {
+                return (
+                  <div key={index} className={styles.tag}>
+                    {condition}
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+        <div className={styles.rightBar}>
+          <ul>
+            <li>ID: {nctId}</li>
+            <li>Date: {statusVerifiedDate}</li>
+            <li>Eligibility: {sex}</li>
+            <li>
+              Location: {city},{state}
+            </li>
+          </ul>
+        </div>
       </div>
     </section>
   );
